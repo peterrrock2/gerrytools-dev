457d6180a10ca04b272c8476a64f3d9ed30f93d2
pizzimathy <pizzimentianthony@gmail.com>
2024-07-17 09:53:22 -0400

    facilities for retrieving VTDs and dual graphs; update dependencies to prevent angry warnings

--- gerrytools/data/geometries.py
+++ gerrytools/data/geometries.py
@@ -1,4 +1,53 @@
+
 import requests
+
+
+def dualgraphs20(state, filepath, geometry="block group"):
+    """
+    Retrieves Lab-processed dual graph data for the provided state and geometry
+    level and writes it to the provided filepath.
+
+    Args:
+        state (us.State): State for which we retrieve data.
+    """
+    acceptable = set("bg", "block group", "blockgroup", "vtd")
+
+    if geometry.lower() not in acceptable:
+        print(f'Requested geometry "{geometry}" is not allowed; ' "loading block groups.")
+        geometry = "bg"
+
+    # Specify the base url.
+    base = "http://data.mggg.org.s3-website.us-east-2.amazonaws.com/dual-graphs/"
+    suffix = (
+        f"{state.abbr.lower()}-{geometry}-connected.json"
+    )
+
+    # Send the request!
+    request = requests.get(base + suffix)
+
+    with open(filepath, "wb") as w:
+        w.write(request.content)
+
+
+def vtds20(state, filepath):
+    """
+    Retrieves Lab-processed geometric data for the provided state and geometry
+    level and writes it to the provided filepath.
+
+    Args:
+        state (us.State): State for which we retrieve data.
+    """
+    # Specify the base url.
+    base = "http://data.mggg.org.s3-website.us-east-2.amazonaws.com/vtd-shapefiles/"
+    suffix = (
+        f"{state.abbr.upper()}_" f"vtd20.zip"
+    )
+
+    # Send the request!
+    request = requests.get(base + suffix)
+
+    with open(filepath, "wb") as w:
+        w.write(request.content)
 
 
 def geometries20(state, filepath, geometry="tract"):
@@ -35,7 +84,7 @@ def geometries20(state, filepath, geometry="tract"):
         geometry = "tract"
 
     # Specify the base url.
-    base = "https://s3.us-east-2.amazonaws.com/data.mggg.org/census-2020/"
+    base = "http://data.mggg.org.s3-website.us-east-2.amazonaws.com/census-2020/"
     suffix = (
         f"{state.abbr.lower()}/{state.abbr.lower()}_" f"{geometrymap[geometry]}.zip"
     )
