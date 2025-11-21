d93fc7b7eaf6483f509f2baa376e0b8cdffa0524
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-11-21 16:13:18 -0700

    Some cleanup of old module stuff

--- gerrytools/data/__init__.py
+++ gerrytools/data/__init__.py
@@ -2,33 +2,23 @@
 Facilities for processing data and districting plans in a standardized fashion.
 """
 
-from .acs import acs5, cvap
-from .AssignmentCompressor import AssignmentCompressor
-from .census import census10, census20, variables
-from .estimatecvap import estimatecvap2010, estimatecvap2020, fetchgeometries
-from .fetch import Submission, submissions, tabularized
+from .census.acs import acs5, cvap
+from .census.census import census
+
+from .districtr.legacy.fetch import (
+    Submission as LegacySubmission,
+    submissions as legacy_submissions,
+    tabularized as legacy_tabularized,
+)
 from .geometries import geometries20, vtds20, dualgraphs20
-from .remap import remap
-from .URLs import csvs, ids, one
 
 __all__ = [
-    "submissions",
-    "tabularized",
-    "remap",
-    "ids",
-    "one",
-    "csvs",
-    "AssignmentCompressor",
-    "Submission",
+    "legacy_submissions",
+    "legacy_tabularized",
+    "LegacySubmission",
     "cvap",
     "acs5",
-    "census20",
-    "variables",
-    "estimatecvap2010",
-    "estimatecvap2020",
-    "fetchgeometries",
-    "census10",
-    "geometries20",
+    "census",
     "vtds20",
-    "dualgraphs20"
+    "dualgraphs20",
 ]
