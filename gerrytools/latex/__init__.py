21c4a5c16e6bb798b17d8c5a8335c8ecbb1b626d
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-12-05 15:54:08 -0700

    Add classes for latex table generation and previewing

---
+++ gerrytools/latex/__init__.py
@@ -0,0 +1,5 @@
+from gerrytools.latex.document import TexDocument
+from gerrytools.latex.table import TexTable
+
+
+__all__ = ["TexDocument", "TexTable"]
---
+++ gerrytools/latex/types.py
@@ -0,0 +1,3 @@
+from typing import Union
+
+ColorLike = Union[str, tuple[int | float, int | float, int | float]]
