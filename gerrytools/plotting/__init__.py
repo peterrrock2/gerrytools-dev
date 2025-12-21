cea17a29473590c2d03609a300209cea25e84365
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2025-12-20 19:11:42 -0700

    Add rough draft sealevel

--- gerrytools/plotting/__init__.py
+++ gerrytools/plotting/__init__.py
@@ -7,7 +7,6 @@ from gerrytools.colors import districtr, flare, latex, purples, redbluecmap
 
 from .annotation import arrow, ideal
 from .scatterplot import scatterplot
-from .sealevel import sealevel
 from .violin import violin
 
 __all__ = [
@@ -20,6 +19,5 @@ __all__ = [
     "redbluecmap",
     "histogram",
     "scatterplot",
-    "sealevel",
     "violin",
 ]
